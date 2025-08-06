9ac6511655c3fd7a18f07ad39c5566b73a5e1b5b
Syed Zayed Ahmed <109905969+Syedzayed@users.noreply.github.com>
2025-08-06 12:32:16 +0530

    implement AI input, template, and special cards with theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import AiInputCard from '@/components/homeCards/AiInputCard'
 import CardCarouselParent from '@/components/homeCards/CardCarouselParent'
+import SpecialCard from '@/components/homeCards/SpecialCard'
+import TemplateCard from '@/components/homeCards/TemplateCard'
+import ThemeChange from '@/components/homeCards/ThemeChange'
 import HeroContent from '@/components/landingPage/HeroContent'
 import Navbar from '@/components/navbar/Navbar'
+import AiInput from '@/components/ui/ai-input'
 import React from 'react'
 
 const page = () => {
@@ -9,6 +14,10 @@ const page = () => {
       <Navbar/>
       <HeroContent />
       <CardCarouselParent />
+      <TemplateCard />
+      <SpecialCard />
+      <AiInputCard />
+      <ThemeChange />
     </div>
   )
 }
